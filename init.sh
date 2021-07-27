d38351e1ec4a41178eac90846c4aa1eb19984fa4
cogito <mukashev.madi@hotmail.com>
2021-07-28 00:52:22 +0600

    Homework 2.1 v2.0

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 # Stepik #
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app &
 gunicorn -c /home/box/web/etc/gunicorn-django.conf.py ask.wsgi:application &
