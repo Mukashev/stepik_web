b0a11e7cfd1ef2ea5c72b77024bebd3f3e7eae17
cogito <mukashev.madi@hotmail.com>
2021-07-28 00:13:51 +0600

    Homework 2.1 v1.7

--- etc/gunicorn-django.conf.py
+++ etc/gunicorn-django.conf.py
@@ -1,4 +1,4 @@
 bind = '0.0.0.0:8000'
 workers = 1
-# pythonpath = '/home/box/web/ask'
-pythonpath = '/home/cogito/nginx_server/ask'
+pythonpath = '/home/box/web/ask'
+# pythonpath = '/home/cogito/nginx_server/ask'
