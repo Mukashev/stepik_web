bd78863b937ff4d7129d7f69a9f45fcd7a6b47cf
cogito <mukashev.madi@hotmail.com>
2021-07-28 02:00:44 +0600

    Homework 2.1 v2.6

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# sudo rm -rf /etc/nginx/sites-enabled/default
+# sudo /etc/init.d/nginx restart
+# gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app &
+# gunicorn -c /home/box/web/etc/gunicorn-django.conf.py ask.wsgi:application &
+
+
+# Test #
+sudo ln -sf ./etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app &
-gunicorn -c /home/box/web/etc/gunicorn-django.conf.py ask.wsgi:application &
+gunicorn -c ./etc/gunicorn.conf.py hello:app &
+gunicorn -c ./etc/gunicorn-django.conf.py ask.wsgi:application &
